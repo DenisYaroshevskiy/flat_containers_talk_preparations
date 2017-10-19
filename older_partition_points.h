36735d6d1caafaea242b93e9706d37cdd324a655
Denis Yaroshevskiy <dyaroshev@yandex-team.ru>
2017-10-19 18:07:10 +0300

    more intermidiate solutions

--- older_partition_points.h
+++ older_partition_points.h
@@ -1,3 +1,5 @@
+#include <cassert>
+
 namespace v1 {
 
 template <typename I, typename P>
@@ -19,4 +21,94 @@ I partition_point_biased(I f, I l, P p) {
 }
 
 }  // namespace v1
+
+namespace v2 {
+
+template <typename I, typename P>
+// requires ForwardIterator<I> && UnaryPredicate<P, ValueType<I>>
+I partition_point_biased(I f, I l, P p) {
+  auto len = std::distance(f, l);
+  if (len < 5)
+    return std::find_if_not(f, l, p);
+
+  auto middle = std::next(f, len / 2);
+  if (p(*middle))
+    return std::partition_point(f, l, p);
+
+  auto step = 1;
+  while (true) {
+    I test = std::next(f, step);
+    if (!p(*test)) {
+      return std::partition_point(f, test, p);
+    }
+    f = ++test;
+    step <<= 1;
+  }
+}
+
+}  // namespace v2
+
+namespace v3 {
+
+template <typename I, typename P>
+// requires ForwardIterator<I> && UnaryPredicate<P, ValueType<I>>
+I partition_point_biased(I f, I l, P p) {
+  // clang-format off
+  if (f == l || !p(*f)) return f; ++f;
+  if (f == l || !p(*f)) return f; ++f;
+  if (f == l || !p(*f)) return f; ++f;
+  if (f == l || !p(*f)) return f; ++f;
+  // clang-format on
+
+  auto len = std::distance(f, l);
+  if (len < 5)
+    return partition_point_biased(f, l, p);
+
+  auto middle = std::next(f, len / 2);
+  if (p(*middle))
+    return std::partition_point(f, l, p);
+
+  auto step = 1;
+  while (true) {
+    I test = std::next(f, step);
+    if (!p(*test)) {
+      return std::partition_point(f, test, p);
+    }
+    f = ++test;
+    step <<= 1;
+  }
+}
+
+}  // namespace v3
+
+namespace v4 {
+
+template <typename I, typename P>
+// requires ForwardIterator<I> && UnaryPredicate<P, ValueType<I>>
+I partition_point_biased(I f, I l, P p) {
+  auto len = std::distance(f, l);
+  auto middle = std::next(f, len / 2);
+  if (p(*middle))
+    return std::partition_point(f, l, p);
+
+  // clang-format off
+  if (!p(*f)) return f; ++f;
+  if (!p(*f)) return f; ++f;
+  if (!p(*f)) return f; ++f;
+  if (!p(*f)) return f; ++f;
+  // clang-format on
+
+  auto step = 1;
+  while (true) {
+    I test = std::next(f, step);
+    if (!p(*test)) {
+      return std::partition_point(f, test, p);
+    }
+    f = ++test;
+    step <<= 1;
+  }
+}
+
+}  // namespace v4
+
 
